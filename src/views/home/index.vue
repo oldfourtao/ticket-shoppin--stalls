6e84f0e32f56f062a6fe0275a91a94cd87e66c18
ios_app <a88@163.com>
2021-04-28 18:02:42 +0800

    update

---
+++ src/views/home/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login-container">
+    <span>拼卷欢乐送</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+    }
+  },
+  destroyed() {
+    // window.removeEventListener('storage', this.afterQRScan)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
